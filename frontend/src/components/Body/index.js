f3fb0dd901f18a61809de54c97f2b01dd00384d7
sindi16 <10029@holbertonstudents.com>
2025-05-08 17:40:40 +0200

    doing carousel part in home page

--- frontend/src/components/Body/index.js
+++ frontend/src/components/Body/index.js
@@ -1,24 +1,45 @@
-import { Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { Button, Card, Container, Row, Col, Carousel } from 'react-bootstrap';
 import carOne from "../../images/car-1.jpg";
-import carTwo from "../../images/car-3.jpg"
+import carTwo from "../../images/car-3.jpg";
+import carThree from "../../images/car-10.jpg";
+import "./index.css"
 
 const BodyHome = () => {
   return (
     <Container>
       <Row>
-        <Col sm={4}>
-          <Card>
-            <Card.Img variant="top" src={`${carOne}`} />
-            <Card.Body>
-              <Card.Title>Featured Vehicles</Card.Title>
-              <Card.Text>
-                "Elegance in Motion"
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
+        <Col sm={8}>
+          <Carousel>
+            <Carousel.Item>
+              <img src={carOne} text="First slide" className='full-width' />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={carTwo} text="Second slide" className='full-width' />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item >
+              <img src={carThree} text="Third slide" className='full-width' />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Col>
-        <Col sm={4}>
+      </Row>
+    </Container>
+  );
+}
+{/* <Col sm={4}>
           <Card>
             <Card.Img variant="top" src={`${carTwo}`} />
             <Card.Body>
@@ -31,11 +52,11 @@ const BodyHome = () => {
         </Col>
         <Col sm={4}>
           <Card>
-            <Card.Img variant="top" src="images/car-1.jpg" />
+            <Card.Img variant="top" src={`${carThree}`} />
             <Card.Body>
-              <Card.Title>Featured Vehicles</Card.Title>
+              <Card.Title>Mercedes Benz</Card.Title>
               <Card.Text>
-                "Elegance in Motion"
+                Elegance in Motion
               </Card.Text>
             </Card.Body>
           </Card>
@@ -43,6 +64,6 @@ const BodyHome = () => {
       </Row>
     </Container>
   );
-}
+} */}
 
 export default BodyHome;
