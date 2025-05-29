f664ea30d6680cd76db2b741e0662bc85560ae0b
sindi16 <10029@holbertonstudents.com>
2025-05-29 14:22:24 +0200

    doing map, run the project into back and frontend in the same time, and trying to creating vehicles and to see the path how it register to the database

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3001", // Adjust this to your frontend URL
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true
+  })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
