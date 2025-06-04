b9a79f8bb0de267a45e22f61ec1f0bbea643e5d5
sindi16 <10029@holbertonstudents.com>
2025-06-04 14:42:43 +0200

    adding vehicle page the form of displaying one vehicle when you click to vehicle product

--- frontend/src/pages/VehiclePage/index.js
+++ frontend/src/pages/VehiclePage/index.js
@@ -2,7 +2,8 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import CustomNavbar from "../../components/Navbar";
 import Footer from '../../components/Footer';
 import Carousel from 'react-bootstrap/Carousel';
-
+import img1 from "../../images/car-1.jpg";
+import img2 from "../../images/car-3.jpg";
 import "./index.css";
 
 const VehiclePage = () => {
@@ -11,51 +12,69 @@ const VehiclePage = () => {
       <div className="vehicle-page-header">
         <CustomNavbar />
       </div>
+
       <div className="page-container">
         <Container>
           <Row>
-            <Col xs={12} sm={5} md={5} className="vehicle-data">
+            {/* Vehicle Data */}
+            <Col xs={12} sm={5} md={4} className="vehicle-data">
               <div className="vehicle-data-inner">
                 <h4>$169 <span>/Per Day</span></h4>
                 <ul>
-                  <li><span className="car-attribute">Doors</span> <span className="car-attr-response">4</span></li>
-                  <li><span className="car-attribute">Passengers</span> <span className="car-attr-response">2</span></li>
-                  <li><span className="car-attribute">Transmission</span> <span className="car-attr-response">Auto</span></li>
-                  <li><span className="car-attribute">Age</span> <span className="car-attr-response">4</span></li>
-                  <li><span className="car-attribute">Luggage</span> <span className="car-attr-response">2</span></li>
-                  <li><span className="car-attribute">Air Condition</span> <span className="car-attr-response">Yes</span></li>
+                  <li><span className="car-attribute">Brand</span> <span className="car-attr-response">BMW</span></li>
+                  <li><span className="car-attribute">Model</span> <span className="car-attr-response">M3</span></li>
+                  <li><span className="car-attribute">Year</span> <span className="car-attr-response">2022</span></li>
+                  <li><span className="car-attribute">Type</span> <span className="car-attr-response">Sedan</span></li>
+                  <li><span className="car-attribute">Color</span> <span className="car-attr-response">Black</span></li>
+                  <li><span className="car-attribute">Description</span> <span className="car-attr-response">Luxury sports car with advanced features</span></li>
                 </ul>
+                <Button variant="warning" className="book-button">Book Now</Button>
               </div>
             </Col>
 
+            {/* Vehicle Carousel */}
             <Col xs={12} sm={7} md={7} className="vehicle-slider">
-              <Carousel className="vehicle-carousel">
-                <Carousel.Item>
-                  <Row className="justify-content-center">
-                    <Col md={10} sm={12} className="mb-4">
-                      <Card className="vehicle-card">
-                        <Card.Img variant="top" src={} />
-                        <Card.Body>
-                          <Card.Title>Luxury Coupe</Card.Title>
-                          <Card.Text className="text-muted">BMW</Card.Text>
-                          <Card.Text>
-                            <span className="price">$169</span>
-                            <span className="text-muted"> /day</span>
-                          </Card.Text>
-                          <div className="d-flex gap-2">
-                            <Button variant="primary" className="w-50">Book now</Button>
-                            <Button variant="success" className="w-50">Details</Button>
-                          </div>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  </Row>
-                </Carousel.Item>
-              </Carousel>
+              <div className="luxury-carousel">
+                <Carousel className="vehicle-carousel">
+
+                  <Carousel.Item>
+                    <Card className="vehicle-card">
+                      <Card.Img variant="top" src={img1} />
+                
+                    </Card>
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <Card className="vehicle-card">
+                      <Card.Img variant="top" src={img1} />
+                    </Card>
+                  </Carousel.Item>
+                  
+                  <Carousel.Item>
+                    <Card className="vehicle-card">
+                      <Card.Img variant="top" src={img1} />
+                    </Card>
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <Card className="vehicle-card">
+                      <Card.Img variant="top" src={img1} />
+                    </Card>
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <Card className="vehicle-card">
+                      <Card.Img variant="top" src={img1} />
+                    </Card>
+                  </Carousel.Item>
+
+                </Carousel>
+              </div>
             </Col>
           </Row>
         </Container>
       </div>
+
       <Footer />
     </>
   );
